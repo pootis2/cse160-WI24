8722161a1286df8a174f828359c63bf46b800c4a
Chengtao Wu <91758627+pootis2@users.noreply.github.com>
2024-02-20 18:34:20 -0800

    Update template.cu

--- PA5/template.cu
+++ PA5/template.cu
@@ -16,6 +16,36 @@
 #define clamp(x) (min(max((x), 0.0), 1.0))
 
 //@@ INSERT CODE HERE
+__global__ void convolution(float *image, const float * __restrict__ mask, float *out, int channels, int width, int height){
+
+    //__shared__ float subInput[w][w];
+
+    //blocks/threads in x and y direction
+    int tx = threadIdx.x; int bx = blockIdx.x;
+    int ty = threadIdx.y; int by = blockIdx.y; 
+
+    int col = bx * width + tx; //col for output/tile
+    int row = by * height + ty; //row for output/tile
+    int xOffset = 0; int imagePixel = 0;
+    int yOffset = 0; int maskValue = 0;
+    if (col < width && row < height){
+        for(int k = 0; k < channels; k++){
+            float accum = 0;
+            for(int y = -Mask_radius; y < Mask_radius+1; y++){
+                for(int x = -Mask_radius; x < Mask_radius+1; y++){
+                    xOffset = col + x;
+                    yOffset = row + y;
+                    if (xOffset >= 0 && xOffset < width && yOffset >= 0 && yOffset < height){
+                        imagePixel = image[(yOffset * width + xOffset) * channels + k];
+                        maskValue = mask[(y+Mask_radius)*Mask_width+x+Mask_radius];
+                        accum += imagePixel * maskValue;
+                    }
+                }
+            }
+            out[(row * width + col)*channels + k] = clamp(accum, 0, 1);
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
   gpuTKArg_t arg;
@@ -59,18 +89,28 @@ int main(int argc, char *argv[]) {
 
   gpuTKTime_start(GPU, "Doing GPU memory allocation");
   //@@ INSERT CODE HERE
+  cudaMalloc((void **) &deviceInputImageData, imageWidth * imageHeight * imageChannels * sizeof(float));
+  cudaMalloc((void **) &deviceOutputImageData, imageWidth * imageHeight * imageChannels * sizeof(float));
+  cudaMalloc((void **) &deviceMaskData, maskColumns * maskRows * sizeof(float));
+
   gpuTKTime_stop(GPU, "Doing GPU memory allocation");
 
   gpuTKTime_start(Copy, "Copying data to the GPU");
   //@@ INSERT CODE HERE
+  cudaMemcpy(deviceInputImageData, hostInputImageData, imageWidth * imageHeight * imageChannels * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceOutputImageData, hostOutputImageData, imageWidth * imageHeight * imageChannels * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceMaskData, hostMaskData, maskColumns * maskRows * sizeof(float), cudaMemcpyHostToDevice);
+
   gpuTKTime_stop(Copy, "Copying data to the GPU");
 
   gpuTKTime_start(Compute, "Doing the computation on the GPU");
   //@@ INSERT CODE HERE
+  dim3 dimBlock(TILE_WIDTH, TILE_WIDTH); 
+  dim3 dimGrid(ceil((float)imageWidth/TILE_WIDTH), ceil((float)imageHeight/TILE_WIDTH)); 
+  convolution<<<dimGrid, dimBlock>>>(hostInputImageData, hostMaskData,
+                                     hostOutputImageData, imageChannels,
+                                     imageWidth, imageHeight);
 
-//  convolution<<<dimGrid, dimBlock>>>(deviceInputImageData, deviceMaskData,
-//                                     deviceOutputImageData, imageChannels,
-//                                     imageWidth, imageHeight);
   gpuTKTime_stop(Compute, "Doing the computation on the GPU");
 
   gpuTKTime_start(Copy, "Copying data from the GPU");
@@ -85,6 +125,9 @@ int main(int argc, char *argv[]) {
   gpuTKSolution(arg, outputImage);
 
   //@@ Insert code here
+  cudaFree(deviceInputImageData);
+  cudaFree(deviceMaskData);
+  cudaFree(deviceOutputImageData);
 
   free(hostMaskData);
   gpuTKImage_delete(outputImage);
